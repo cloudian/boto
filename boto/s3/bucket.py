2ae23ccb9909fea33d625eb06b44b5f4276804e4
Masashi Ozawa <mozawa@cloudian.com>
2017-08-17 17:12:39 +0900

    Amazon AWS S3 - Add Object tagging feature

--- boto/s3/bucket.py
+++ boto/s3/bucket.py
@@ -233,6 +233,7 @@ class Bucket(object):
             k.handle_replication_headers(response)
             k.handle_restore_headers(response)
             k.handle_storage_class_header(response)
+            k.handle_tagging_count_headers(response)
             k.handle_addl_headers(response.getheaders())
             return k, response
         else:
